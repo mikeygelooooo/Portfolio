3e612ee64dc98bc8302ad8539cb7448a336b2618
mikeygelooooo <mikeygelo7@gmail.com>
2025-06-20 09:32:41 +0800

    Basic Layout for Home, About, and Skills Sections

--- resources/views/sections/about.blade.php
+++ resources/views/sections/about.blade.php
@@ -1,5 +1,21 @@
-<div class="container p-5">
-    <h1>About</h1>
+<div class="container d-flex flex-column justify-content-center p-5" style="min-height: 105vh;">
+    <h2 class="text-center text-uppercase mb-5 fw-bold">
+        <i class="fas fa-user me-3"></i>About Me
+    </h2>
 
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt totam praesentium quisquam laborum reprehenderit vitae dolor sunt aspernatur! Dolorum sed, iusto odit nihil illum consectetur. Nam sapiente quibusdam, ipsum incidunt consequatur nulla laboriosam repellendus voluptas ipsa sed debitis dolorum consequuntur beatae repellat harum cumque labore quisquam magnam minus exercitationem placeat eos enim. Corporis modi adipisci nostrum, totam debitis inventore dolor. Optio quae delectus iure cupiditate est ipsam soluta illum iusto eligendi fuga deserunt, reiciendis maxime similique inventore fugit earum aperiam corporis nesciunt, distinctio facere repudiandae voluptates? Tenetur, iste deleniti! Expedita ducimus vitae commodi placeat, voluptas aliquam perspiciatis aspernatur earum recusandae?</p>
+    <div class="row w-100">
+        <div class="col-md-6 d-flex justify-content-center mb-4">
+            <img src="{{ asset('images/about-photo.jpeg') }}" alt="About Photo" class="img-fluid rounded-5 shadow-lg" style="max-width: 350px; height: auto;">
+        </div>
+        <div class="col-md-6">
+            <h3>I'm Michael Angelo Ochengco</h3>
+            <h4>Back-end Developer</h4>
+            <p class="mt-3">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit expedita blanditiis porro officiis, minima quis a. Cupiditate molestias mollitia omnis excepturi atque sequi provident in commodi. Deserunt in harum laborum ipsum voluptatum vitae sequi ratione natus accusantium enim praesentium, voluptas aspernatur obcaecati rerum optio sunt ex, dolor tempora laudantium! Quaerat.
+            </p>
+            <a href="#" class="btn btn-primary btn-lg py-2 px-4 mt-3">
+                Resume <i class="fas fa-circle-down ms-2"></i>
+            </a>
+        </div>  
+    </div>
 </div>
--- resources/views/sections/skills.blade.php
+++ resources/views/sections/skills.blade.php
@@ -1,5 +1,113 @@
-<div class="container p-5">
-    <h1>Skills</h1>
+<div class="container text-center d-flex flex-column justify-content-center p-5" style="min-height: 105vh;">
+    <h2 class="text-uppercase mb-5 fw-bold">
+        <i class="fas fa-laptop-code me-3"></i>Skills and Abilities
+    </h2>
 
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt totam praesentium quisquam laborum reprehenderit vitae dolor sunt aspernatur! Dolorum sed, iusto odit nihil illum consectetur. Nam sapiente quibusdam, ipsum incidunt consequatur nulla laboriosam repellendus voluptas ipsa sed debitis dolorum consequuntur beatae repellat harum cumque labore quisquam magnam minus exercitationem placeat eos enim. Corporis modi adipisci nostrum, totam debitis inventore dolor. Optio quae delectus iure cupiditate est ipsam soluta illum iusto eligendi fuga deserunt, reiciendis maxime similique inventore fugit earum aperiam corporis nesciunt, distinctio facere repudiandae voluptates? Tenetur, iste deleniti! Expedita ducimus vitae commodi placeat, voluptas aliquam perspiciatis aspernatur earum recusandae?</p>
+    <div class="card shadow bg-light">
+        <div class="card-body">
+            <div class="row g-4">
+                <!-- Languages -->
+                <div class="col-6 col-sm-4 col-md-3 col-lg-2">
+                    <div class="p-3 border rounded text-center bg-body-tertiary shadow-sm h-100">
+                        <i class="devicon-python-plain fs-1"></i>
+                        <div class="mt-2 fs-6">Python</div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-4 col-md-3 col-lg-2">
+                    <div class="p-3 border rounded text-center bg-body-tertiary shadow-sm h-100">
+                        <i class="devicon-php-plain fs-1"></i>
+                        <div class="mt-2 fs-6">PHP</div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-4 col-md-3 col-lg-2">
+                    <div class="p-3 border rounded text-center bg-body-tertiary shadow-sm h-100">
+                        <i class="devicon-html5-plain fs-1"></i>
+                        <div class="mt-2 fs-6">HTML</div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-4 col-md-3 col-lg-2">
+                    <div class="p-3 border rounded text-center bg-body-tertiary shadow-sm h-100">
+                        <i class="devicon-css3-plain fs-1"></i>
+                        <div class="mt-2 fs-6">CSS</div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-4 col-md-3 col-lg-2">
+                    <div class="p-3 border rounded text-center bg-body-tertiary shadow-sm h-100">
+                        <i class="devicon-javascript-plain fs-1"></i>
+                        <div class="mt-2 fs-6">JavaScript</div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-4 col-md-3 col-lg-2">
+                    <div class="p-3 border rounded text-center bg-body-tertiary shadow-sm h-100">
+                        <i class="devicon-c-plain fs-1"></i>
+                        <div class="mt-2 fs-6">C</div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-4 col-md-3 col-lg-2">
+                    <div class="p-3 border rounded text-center bg-body-tertiary shadow-sm h-100">
+                        <i class="devicon-java-plain fs-1"></i>
+                        <div class="mt-2 fs-6">Java</div>
+                    </div>
+                </div>
+
+                <!-- Frameworks -->
+                <div class="col-6 col-sm-4 col-md-3 col-lg-2">
+                    <div class="p-3 border rounded text-center bg-body-tertiary shadow-sm h-100">
+                        <i class="devicon-django-plain fs-1"></i>
+                        <div class="mt-2 fs-6">Django</div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-4 col-md-3 col-lg-2">
+                    <div class="p-3 border rounded text-center bg-body-tertiary shadow-sm h-100">
+                        <i class="devicon-laravel-plain fs-1"></i>
+                        <div class="mt-2 fs-6">Laravel</div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-4 col-md-3 col-lg-2">
+                    <div class="p-3 border rounded text-center bg-body-tertiary shadow-sm h-100">
+                        <i class="devicon-codeigniter-plain fs-1"></i>
+                        <div class="mt-2 fs-6">CodeIgniter</div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-4 col-md-3 col-lg-2">
+                    <div class="p-3 border rounded text-center bg-body-tertiary shadow-sm h-100">
+                        <i class="devicon-bootstrap-plain fs-1"></i>
+                        <div class="mt-2 fs-6">Bootstrap</div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-4 col-md-3 col-lg-2">
+                    <div class="p-3 border rounded text-center bg-body-tertiary shadow-sm h-100">
+                        <i class="devicon-vuejs-plain fs-1"></i>
+                        <div class="mt-2 fs-6">Vue.js</div>
+                    </div>
+                </div>
+
+                <!-- Tools & Skills -->
+                <div class="col-6 col-sm-4 col-md-3 col-lg-2">
+                    <div class="p-3 border rounded text-center bg-body-tertiary shadow-sm h-100">
+                        <i class="devicon-github-plain fs-1"></i>
+                        <div class="mt-2 fs-6">GitHub</div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-4 col-md-3 col-lg-2">
+                    <div class="p-3 border rounded text-center bg-body-tertiary shadow-sm h-100">
+                        <i class="devicon-git-plain fs-1"></i>
+                        <div class="mt-2 fs-6">Git</div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-4 col-md-3 col-lg-2">
+                    <div class="p-3 border rounded text-center bg-body-tertiary shadow-sm h-100">
+                        <i class="devicon-mysql-plain fs-1"></i>
+                        <div class="mt-2 fs-6">MySQL</div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-4 col-md-3 col-lg-2">
+                    <div class="p-3 border rounded text-center bg-body-tertiary shadow-sm h-100">
+                        <i class="devicon-sqlite-plain fs-1"></i>
+                        <div class="mt-2 fs-6">SQLite</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
